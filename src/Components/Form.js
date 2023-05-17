174007bfa6a09f7b47d158b3a3ae24799d95bf8f
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-17 09:08:37 +0100

    start

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,7 +3,16 @@ import React from 'react'
 const Form = () => {
  return (
   <div>
-   
+   <label htmlFor="name">Fullnames</label>
+  <input type="text" name="name" id="name"/>
+   <label htmlFor="name">Phone</label>
+   <input type="tel" name="phone" id="phone" />
+   <label htmlFor="name"><option>Sex</option></label>
+   <input type="text" name="name" id="name" />
+   <label htmlFor="name">Name</label>
+   <input type="text" name="name" id="name" />
+   <label htmlFor="name">Name</label>
+   <input type="text" name="name" id="name" />
   </div>
  )
 }
